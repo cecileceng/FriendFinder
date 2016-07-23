d4a213300567f186ba75335ff6d177345fd749fa
Cecile Nguyen <cecile.ceng@gmail.com>
2016-07-23 17:27:59 -0500

    trying to restore the glory of heroku

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -3,13 +3,21 @@ var friendData = require('../data/friends.js')
 var path = require('path');
 
 // ROUTING
-module.exports = function(app) {
+var API = function(app) {
+	this.get = function() { 
 	app.get('/api/friends', function(req,res){
 		res.json(friendData);
-});
-
-	}
-	app.post('/api/friends', function(req, res){
-
 	})
 };
+/*
+	app.post('api/friends', function(req,res){
+		//add data from request to friendData array
+		//calculate animal here and save in friendData
+	})
+	app.post('/api/clear', function(req, res){
+		friendData = [];
+		console.log(friendData);
+	})*/
+};
+
+module.exports = API;
