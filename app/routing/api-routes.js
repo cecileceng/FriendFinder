a6f03cbd98af1ea9b90c0e8e933d5bdad3e8dcf8
Cecile Nguyen <cecile.ceng@gmail.com>
2016-07-23 14:16:58 -0500

    herokuuuuu

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -3,10 +3,13 @@ var friendData = require('../data/friends.js')
 var path = require('path');
 
 // ROUTING
-module.exports = function(app) {
+var API = function(app) {
+	this.get = function() { 
 	app.get('/api/friends', function(req,res){
 		res.json(friendData);
 	})
+};
+/*
 	app.post('api/friends', function(req,res){
 		//add data from request to friendData array
 		//calculate animal here and save in friendData
@@ -14,5 +17,7 @@ module.exports = function(app) {
 	app.post('/api/clear', function(req, res){
 		friendData = [];
 		console.log(friendData);
-	})
-}
+	})*/
+};
+
+module.exports = API;
